373ad8ec21b8e4bf5015e86c30c386d7a8274a7a
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-03 10:48:53 +0100

    cleaned up the code and added JSdoc

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ function main() {
     startSlideshowAnimation();
     
 }
+/** Start the animation with a set interval */
 function startSlideshowAnimation() {
     setInterval(changeActiveImage, 2000);
     changeActiveImage();
 }
 
-
+/** Presents the next image by checking which image is currently presented */
 function changeActiveImage() {
     const images = document.querySelectorAll('.slideshow-container > img');
    
@@ -23,17 +24,15 @@ function changeActiveImage() {
         //1. ta bort show-klassen från aktuellt index
         images[i].classList.remove('show');
 
-        //2. lägg till show-klassen på nästa index
-        let nextIndex = i + 1;
+            //2. Make sure we go back to index 0 when at the end of array.
+            let nextIndex = i + 1;
+            if (nextIndex >= images.length) {
+                nextIndex = 0;
+            }
 
-        if (nextIndex >= images.length) {
-            nextIndex = 0;
-        }
-
-        images[nextIndex].classList.add('show');
-
-        //3. hoppa ur loopen när vi bytt bild
-        break;
+            images[nextIndex].classList.add('show');
+            //3. hoppa ur loopen när vi bytt bild
+            break;
         }
     }
 }
